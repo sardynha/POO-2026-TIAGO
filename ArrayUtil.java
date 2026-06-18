f654192876329058e6f9052304c091b26d7699a6
sarah <sahpacheco321@gmail.com>
2026-06-18 20:31:43 -0300

    adicionando_min_max

--- ArrayUtil.java
+++ ArrayUtil.java
@@ -15,4 +15,46 @@ public class ArrayUtil {
         return contador;
     }
 
+    //static pq nao precisa criar um objeto da classe arrayutil
+    //comparable: o t pode ser qualquer tipo, desde que saiba se comparar com outro t
+
+        public static <T extends Comparable<T>> T max(T[] vetor) {
+
+         verificaExiste(vetor);
+        
+
+//aqui tem que assumir que o primeiro é o maior
+T maior = vetor[0];
+
+for (T item : vetor) {
+//a cada item do vetor
+if (item.compareTo(maior) > 0) {
+    maior = item;
+}
+}
+return maior;
+
+    }
+
+    public static <T extends Comparable<T>> T min(T[] vetor) {
+
+        verificaExiste(vetor);
+
+        T menor = vetor[0];
+
+        for(T item : vetor) {
+            if(item.compareTo(menor) < 0) {
+                menor = item;
+            }
+        }
+        return menor;
+    }
+
+    public static <T> void verificaExiste(T[] vetor) {
+     //verificando se o vetor existe para evitar uma repeticao rsrsrs
+
+    if (vetor == null || vetor.length == 0) {
+        throw new IllegalArgumentException("o array está vazio ou null");
+    }
+}
 }
